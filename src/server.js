f74b1fffb0d1da36c655346e9cd8e09d3df69a64
Ja'far <ajib.aiwa@gmail.com>
2025-01-18 11:03:28 +0700

    refactor:all - Restructure Hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import Hapi from '@hapi/hapi';
-import routes from './routes.js';
+import notes from './api/notes/index.js';
+import NotesService from './services/inMemory/NotesService.js';
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server(
     {
       port: 5000,
@@ -14,7 +16,12 @@ const init = async () => {
     }
   );
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
